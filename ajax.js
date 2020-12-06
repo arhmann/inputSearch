d977bec571d8728ebf1454700295cb8af0706638
iurii.a <iu.arhmann@gmail.com>
2020-12-06 15:56:37 +0300

    fix

--- ajax.js
+++ ajax.js
@@ -1,24 +1,23 @@
-let xhttp = new XMLHttpRequest();
-let originalURL = "https://raw.githubusercontent.com/cscart/apply-for-job/master/frontend-developer/files/003-rate-areas.json";
-let ulItems = document.querySelector(".content__list");
-let objCityName = {};
 
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
+let arrCityNames = {};
+let UlParrent = document.querySelector('.content__list');
+
+fetch('https://raw.githubusercontent.com/cscart/apply-for-job/master/frontend-developer/files/003-rate-areas.json',{
+  method: 'GET',
+})
+.then(response => {
+  if(response.ok == true){
+    return response.text()
   }
-};
+})
+.then(response => {  //string
+  arrCityNames = JSON.parse(response);
+   for(i in arrCityNames){
+    let itemDOM = `<li> <span> ${arrCityNames[i].name} </span> <button class="content__btn btnAdd">Добавить</button></li>`;
+    console.log(arrCityNames[i].name)
+    UlParrent.innerHTML += itemDOM;
+   }
+   
+     
+})
 
-xhttp.open("GET", originalURL, true);
-xhttp.send();
-xhttp.onload = function () {
-  let responseAjax = xhttp.response;
-  let resAjax = JSON.parse(responseAjax);
-
-  for (indx in resAjax) {
-    objCityName = resAjax[indx].name;
-         let itemDOM = `<li> <span> ${resAjax[indx].name} </span> <button class="content__btn btnAdd">Добавить</button></li>`;
-        ulItems.innerHTML += itemDOM; 
-
-     //ul.appendChild(ulItems)
-  }
-};
--- app.js
+++ app.js
@@ -1,50 +1,57 @@
-const input = document.querySelector('.content__input');
 
-input.addEventListener('input', function (e) {
- e.preventDefault();
-   let val = this.value.trim();
-   let itemsCityName = document.querySelectorAll('.content__list li span');
-   let itemsList = document.querySelectorAll('.content__list li');
-   let btnsAdd = document.querySelectorAll('.btnAdd');
 
-   if(val != ''){
+input.addEventListener("input", function (e) {
+  e.preventDefault();
+  const input = document.querySelector(".content__input");
+  let val = this.value.trim();
+  let itemsList = document.querySelectorAll(".content__list li");
+  let itemsCityName = document.querySelectorAll(".content__list li span");
 
-    itemsList.forEach(function(item){
-     //console.log(item.textContent )
+  if (val != "") {
+  
+    itemsCityName.forEach(function (city) {
+      if (city.innerText.search(val) == -1) {
+        itemsList.forEach(function (item) {
+          item.classList.add("hide");
+        });
+        city.classList.add("hide");
+        city.innerHTML = city.innerText;
+      } else {
+        city.classList.remove("hide");
+        let str = city.innerText;
+        city.innerHTML = insertMark(
+          str,
+          city.innerText.search(val),
+          val.length
+        );
+      }
+    });
+  } else {
+    itemsCityName.forEach(function (city) {
+      city.classList.remove("hide");
+      city.innerHTML = city.innerText;
+    });
+  }
+
+});
+
+function insertMark(string, pos, len) {
+  return (
+    string.slice(0, pos) +
+    "<mark>" +
+    string.slice(pos, pos + len) +
+    "</mark>" +
+    string.slice(pos + len)
+  );
+}
+
+  /*   itemsList.forEach(function(item){
   
      itemsCityName.forEach(function(city){
          if (city.textContent == val ) {
             console.log('совпадение')
          }
-       // console.log(city.textContent )
       })
-    });
+    }); */
 
-    itemsCityName.forEach(function(city){
-        if (city.innerText.search(val) == -1){
-            itemsList.forEach(function(item){
-                item.classList.add('hide');
-             });
-            city.classList.add('hide'); 
-            city.innerHTML = city.innerText;
-        } else {
-            city.classList.remove('hide');
-            /* for(let i = 0; i < itemsList.length; i++){
-                itemsList[i].style.display = 'flex';
-             } */
-            let str = city.innerText;
-            city.innerHTML = insertMark(str, city.innerText.search(val), val.length)
-        }
-    });
-   } else {
-    itemsCityName.forEach(function(city){
-        city.classList.remove('hide');
-        city.innerHTML = city.innerText;
-     })
-  }
-});
-
-function insertMark(string, pos, len){
-    return string.slice(0, pos) + '<mark>' + string.slice(pos, pos+len) + '</mark>' + string.slice(pos + len);
-}
-
+   
