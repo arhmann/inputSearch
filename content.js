b03c4231fa3a0b2b99414b99ffeab91ada09e3de
iurii.a <iu.arhmann@gmail.com>
2020-12-11 19:40:17 +0300

    fix

--- content.js
+++ content.js
@@ -3,7 +3,8 @@ import Api from "./api.js";
 export default class Content extends Api {
   constructor() {
     super();
-    this.url = "https://raw.githubusercontent.com/cscart/apply-for-job/master/frontend-developer/files/003-rate-areas.json";
+    this.url =
+      "https://raw.githubusercontent.com/cscart/apply-for-job/master/frontend-developer/files/003-rate-areas.json";
   }
 
   searchValue() {
@@ -11,38 +12,32 @@ export default class Content extends Api {
     const res = api.fetchData(this.url);
     const arrNames = [];
     const input = document.querySelector(".content__input");
-    let result = null;
+
     res.then(function (ajaxNames) {
       ajaxNames.forEach((names) => {
         arrNames.push(names.name);
+        const content = new Content();
 
         input.addEventListener("input", () => {
           if (input.value === "" || input.value.length < 3) return;
-          const matches = arrNames.filter((city) =>
-            city.toLowerCase().includes(input.value.toLowerCase()));
-            matches.forEach((cityName) => {
-            if (matches.includes(cityName)) {
-              result = cityName;
-            }
-          });
-            const content = new Content();
-            content.createElement(result);
-
+           const matches = arrNames.filter((cities) =>
+            cities.toLowerCase().includes(input.value.toLowerCase())
+          );
+           content.render(matches);
         });
       });
     });
   }
 
-  createElement(element) {
-    
+  render(elements) {
     const ulItem = document.querySelector(".content__list");
+    ulItem.innerHTML = '';
     let nameCity = document.createElement("li");
-    nameCity.innerHTML = `<span class="content__city-name"> ${element} </span> <button class="content__btn btnAdd">Добавить</button>`;
-    ulItem.appendChild(nameCity);
-    nameCity.setAttribute("class", "content__item");
-    /* if (document.querySelector('.content__city-name').textContent.includes(element)) {
-      return;
-    }  */
-      
+    if(nameCity)
+    elements.forEach(el => {
+      nameCity.innerHTML = `<span class="content__city-name"> ${el} </span> <button class="content__btn btnAdd">Добавить</button>`;
+      ulItem.appendChild(nameCity);
+      nameCity.setAttribute("class", "content__item");
+    })    
   }
 }
